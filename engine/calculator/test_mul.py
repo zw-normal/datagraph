76fa9ad8009e3af0be7dfbd70382a9f12c31e387
Wei Zheng <wade.zheng.pro@aliyun.com>
2020-09-15 21:49:22 +1000

    Add test coverage

---
+++ engine/calculator/test_mul.py
@@ -0,0 +1,25 @@
+from graph.models import DataNodeType
+from engine.calculator.mul import Form as MultiplicationForm
+from engine.tests import CommonTestCase
+
+
+class MultiplicationTestCase(CommonTestCase.CalculatorTestCase):
+    node_name = 'mul'
+    node_type = DataNodeType.CALCULATOR
+    node_params = {
+        'multiplier': 0.001,
+    }
+    form_class = MultiplicationForm
+
+    @property
+    def special_form_fields(self):
+        return {
+            'multiplier': 0.001,
+        }
+
+    def assertSpecialFormFields(self, form):
+        self.assertEqual(
+            form.data['multiplier'], 0.001)
+
+    def assertSpecialFields(self, node):
+        self.assertDictEqual(node.params, self.node.params)
